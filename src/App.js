de9872beb410398224ee06caed7ce20112a9fc84
Damien Metzler <dmetzler@amazon.fr>
2022-10-14 10:11:13 +0200

    Add message

--- src/App.js
+++ src/App.js
@@ -31,7 +31,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {message}
+        </p>
+        <p>
+          {error}
         </p>
         <a
           className="App-link"
